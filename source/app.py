704fb980739053c475e80aec0175378828c9b80c
Ankita Haldia <70904567+ankitahaldia@users.noreply.github.com>
2020-12-03 17:14:10 +0530

    only for mandatory features

---
+++ source/app.py
@@ -0,0 +1,37 @@
+from flask import Flask,request, jsonify
+import DataFeatures
+from source.preprocessing import cleaning_data
+from source.predict import prediction
+
+app = Flask(__name__)
+
+@app.route('/')
+def status():
+    return 'Alive'
+
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+    if request.method == 'GET' :
+        #returning a string to explain what the POST expect (data and format)
+        retjson = {
+            "area": 'int',
+            "property-type": 'APARTMENT | HOUSE | OTHERS',
+            "rooms-number": 'int',
+            "zip-code": 'int'
+                 }
+        return jsonify(retjson)
+
+    else :
+        datadict = request.get_json()
+        data = DataFeatures()
+        errors = data.validate(datadict)
+        if errors:
+            abort(BAD_REQUEST, str(errors)) 
+        get the required parameters
+        processeddata = cleaning_data.preprocess(datadict)
+        prediction = prediction.predict_price(processeddata)
+        return jsonify(prediction)
+        
+
+if __name__ == "__main__":
+    app.run()
