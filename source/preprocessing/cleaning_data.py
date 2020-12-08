c2c92bb59dd149550876576715e8b94642d1fdb5
phfimmers <philippefimmers@gmail.com>
2020-12-08 09:53:05 +0100

    impute missing features draft

--- source/preprocessing/cleaning_data.py
+++ source/preprocessing/cleaning_data.py
@@ -1,57 +1,74 @@
 from typing import Dict, Optional, Union
+import pandas as pd
+import json
 
 def preprocess(house_data: Optional[Dict[str, Union[int, bool, str]]] = None) -> Union[Dict[str, Union[int, bool, str]], str]:
-	'''Takes the parsed json data provided by the frontend and returns a dictionary that conforms to the X input
-	features of our model, imputing values for optional fields where needed. If the required json format was not
-	adhered to, the field with the error is returned (ex. 'area_validation_error').
-	:house_data: the parsed json data provided as a dictionary (output of request.get_json())
+	'''Takes the parsed and validated json data provided in the request
+	and returns a dictionary that conforms to the X input features of
+	our model.
+	:house_data: the validated dictionary of the request
 	'''
 
-	# for development, missing house_data is allowed, using below one instead
-	if house_data == None:
-		house_data = {
-			'area': 100,
-			'property-type': 'APARTMENT',
-			'rooms-number': 3,
-			'zip-code': 2650
-		}
+	# create a dataframe out of the request for easier manipulation
+	# ensure the right dummies can be creatd by adding an observation
+	# with inverse booleans	
+	for key, value in house_data.items():
+		if type(value) == bool:
+			house_data[key] = [value, not value]
+		else:
+			house_data[key] = [value, value]
 
-	mandatories_ref = {
-		'area': int,
-		'property-type': ['APARTMENT', 'HOUSE', 'OTHERS'],
-		'rooms-number': int,
-		'zip-code': int
-	}
+	request = pd.DataFrame.from_dict(house_data)
 
-	optionals_ref = {
-		'land-area': int,
-		'garden': bool,
-		'garden-area': int,
-		'equipped-kitchen': bool,
-		'full-address': str,
-		'swimmingpool': bool,
-		'furnished': bool,
-		'open-fire': bool,
-		'terrace': bool,
-		'terrace-area': int,
-		'facades-number': int,
-		'building-state': ['NEW', 'GOOD', 'TO RENOVATE', 'JUST RENOVATED', 'TO REBUILD']
-	}
 
-	# check if mandatory items are present and of the correct type
-	for key, value in mandatories_ref.items():
-		try:
-			# print(f"starting {key},{value} try")
-			if type(house_data[key]) == value:
-				# print(f"{key}, {value} validated")
-				pass
-			else:
-				# print(f"{key} not of {value}")
-				pass
-		except:
-			# print(f"{key}, {value} not present")
-			pass
+	# dummy encode categoricals
+	dummies = ["property_type", "zip_code", "garden",
+					"equipped_kitchen", "swimmingpool",
+					"furnished", "open_fire", "terrace",
+					"facades_number", "building_state"]
+	request = pd.get_dummies(data = request, columns = dummies,
+								prefix_sep = '')
 
-	# check if mandatory categoricals are correctly spelled
+
+	# adopt naming used in our model ('-' iso '_')
+	bool_rename = {"gardenTrue": "garden",
+				"equipped_kitchenTrue": "equipped_kitchen",
+				"swimmingpoolTrue": "swimmingpool",
+				"furnishedTrue": "furnished",
+				"open_fireTrue": "open_fire",
+				"terraceTrue": "terrace"}
+	request.rename(columns = bool_rename, inplace = True)
+
+	categoricals = ["property_type", "zip_code", "building_state"]
+	for cat in categoricals:
+		request.columns = request.columns.str.replace(cat, '')
+
+	request.columns = request.columns.str.replace('_', '-')
+
+	
+	# Set up merging of the request data into the structure
+	# of our model. Missing Optional features will be 0.
+	with open("source/model/columns.json","r") as f:
+		model_columns = json.load(f)['data_columns']
+	model_df = pd.DataFrame({col: 0 for col in model_columns},
+				index = [0]).T
+	request_df = request.T[[0]]
+	
+	request = request_df.merge(model_df, how = 'right',
+							right_index = True,
+							left_index = True)
+	request['request'] = request.sum(axis = 1).astype(int)
+	request = request['request'].to_numpy()
+	print(request)
 
 	return house_data
+
+
+example = {'open_fire': True, 'garden': True, 'facades_number': 2,
+		'full_address': 'Monseigneur Cardijnlaan 33/2 2650 Edegem',
+		'land_area': 500, 'zip_code': 2650, 'property_type': 'APARTMENT',
+		'garden_area': 200, 'building_state': 'NEW', 'area': 100,
+		'terrace': True, 'terrace_area': 10, 'equipped_kitchen': True,
+		'furnished': False, 'rooms_number': 3, 'swimmingpool': False}
+
+preprocess(example)
